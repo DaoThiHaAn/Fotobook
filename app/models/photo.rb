cbd40c145df59c227ac5d0d0bdade4ee5ba1ff15
Dao Thi Ha An <daothihaan@gmail.com>
2025-07-25 20:54:27 +0700

    add photo/avatar uploader

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,8 @@ class Photo < ApplicationRecord
   validates :title, presence: true, length: { maximum: 140 }
   validates :description, length: { maximum: 300 }
 
+  mount_uploader :image_path, PhotoUploader
+
   has_many :album_components, dependent: :destroy
   has_many :albums, through: :album_components
   belongs_to :profile, primary_key: :user_id, foreign_key: :user_id, counter_cache: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  validates :email,  presence: true, uniqueness: true, length: {maximum: 25}, on: :create
-  validates :lname, :fname, presence: true, length: {maximum: 25}
-  
+  validates :email,  presence: true, uniqueness: true, length: { maximum: 25 }, on: :create
+  validates :lname, :fname, presence: true, length: { maximum: 25 }
+
+  mount_uploader :avatar, AvatarUploader
+
   has_one :profile, dependent: :destroy
 end
