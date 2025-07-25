88db33fc6a8320f092ff3e0d961fe393db915ca9
Dao Thi Ha An <daothihaan@gmail.com>
2025-07-26 00:41:09 +0700

    custom resend email form + send email confirmation + authorize navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def formatted_name(user)
+    # Format: An B. C. Dao Thi Ha
+    fname_array = user.first_name.split
+    lname_array = user.last_name.split
+    fname = fname_array.shift.capitalize
+
+    fname_array = fname_array.map { |word|
+      word[0].upcase + "."
+    }
+
+    lname_array = lname_array.map { |word| word.capitalize }
+
+    "#{fname} #{fname_array.join(' ')} #{lname_array.join(' ')}"
+  end
 end
