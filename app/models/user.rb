5ba15e21fe58b36c831382089d1c2fc2ba362304
Dao Thi Ha An <daothihaan@gmail.com>
2025-07-27 01:32:30 +0700

    custom error display in signup form, add password format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,18 @@ class User < ApplicationRecord
   validates :last_name, :first_name, presence: true, length: { in: 2..25 }
 
   has_one :profile, dependent: :destroy
+
+  # Override Devise's default password validation
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    # Example: at least 8 characters, one lowercase, one uppercase, one digit
+    unless password.match?(/\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}\z/)
+      errors.add :password, :invalid_password_format
+    end
+  end
 end
