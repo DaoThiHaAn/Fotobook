731f99996f108e5473aa0106bb4b5e2537604944
Dao Thi Ha An <daothihaan@gmail.com>
2025-08-01 08:32:37 +0700

    change to deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "example.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.smtp_settings = {
     user_name: Rails.application.credentials.dig(:smtp, :user_name),
     password: Rails.application.credentials.dig(:smtp, :password),
